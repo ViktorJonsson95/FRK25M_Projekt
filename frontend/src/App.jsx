75ef269d6a25b345559daf86d7acee9aca97d723
ViktorJonsson95 <jonsson.viktor@gmail.com>
2026-03-05 13:12:24 +0100

    la till todoList i App.jsx och la till import till getServices

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,10 +1,12 @@
 import AddTodo from "./components/AddTodo";
+import TodoList from "./components/TodoList";
 
 function App() {
   return (
     <>
       <h1>Todo App</h1>
       <AddTodo />
+      <TodoList />
     </>
   );
 }
