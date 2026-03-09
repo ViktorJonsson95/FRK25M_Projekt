dea25d3738cb4d75d0d50e5a0f87ce51e14852d0
SanjaStosic <s.a.nj.i.c.a_94@hotmail.com>
2026-03-09 15:08:05 +0100

    Fixade URL lagt till try och catch och returnerar data

--- frontend/src/services/updateStatus.js
+++ frontend/src/services/updateStatus.js
@@ -1,12 +1,22 @@
-export async function updateStatus(id, completed) { 
-    const response = await
-        fetch(`http://localhost:3000/todos/${id}`, { // id är unikt för varje todo, så vi kan uppdatera rätt todo.
-            method: "PATCH", // säger till server att vi vill uppdatera en del av datan (i det här fallet, bara "completed" statusen)
+
+export async function updateStatus(id, completed) {
+    try {
+        const response = await fetch(`http://localhost:3000/Todo/${id}`, {
+            method: "PUT",
             headers: {
                 "Content-Type": "application/json",
             },
-            body: JSON.stringify({ completed }),// skickar den nya "completed" statusen till backend, som är antingen true eller false.
+            body: JSON.stringify({ completed })
         });
 
-    return response.json();
+        if (!response.ok) {
+            throw new Error("Failed to update todo");
+        }
+
+        const data = await response.json();
+        return data;
+
+    } catch (error) {
+        console.error("Error updating todo:", error);
+    }
 }
