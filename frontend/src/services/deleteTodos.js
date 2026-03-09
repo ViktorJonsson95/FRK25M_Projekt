cd581e838685d67e9232fd82419027cba65f0141
corper18 <219812797+corper18@users.noreply.github.com>
2026-03-09 22:59:53 +0100

    Lägger till delete-funktion av todos från UI, backend och databas

---
+++ frontend/src/services/deleteTodos.js
@@ -0,0 +1,24 @@
+const api_url = "http://localhost:3000";
+
+// Funktion som skickar en DELETE request till backend för att ta bort en todo
+export async function deleteTodo(id) {
+    try {
+        // Skickar request till endpointen DELETE /Todo/:id
+        // id sätts in i URL:en så backend vet vilken todo som ska tas bort
+        const response = await fetch(`${api_url}/Todo/${id}`, {
+            method: "DELETE",
+        });
+
+        //Vårt felmeddelande
+        if(!respone.ok) {
+            throw new Error("could not delete todo");
+        }
+        //Här returneras svaret från backend
+        return await respone.json();
+    } catch (error) {
+
+        console.error("Delete error:", error);
+        // Skickar vidare felet så komponenten som anropar funktionen kan hantera det
+        throw error;
+    }
+}
